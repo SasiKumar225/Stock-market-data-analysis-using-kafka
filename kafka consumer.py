92b83b15bf95aed6643a2ecb943176f4773e8e6e
Sasi Nagubadi <80870715+SasiKumar225@users.noreply.github.com>
2026-06-16 09:39:42 -0500

    Update kafka consumer.py

--- kafka consumer.py
+++ kafka consumer.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 from kafka import KafkaConsumer
 from time import sleep
-from json import dumps,loads
+from json import dumps, loads
 import json
 from s3fs import S3FileSystem
-
-
-# In[10]:
-
 
 consumer = KafkaConsumer(
     'demo_testing2',
@@ -20,29 +10,12 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 
-# In[11]:
-
-
 s3 = S3FileSystem()
-
-
-# In[12]:
-
 
 #create an s3 bucket 
 for count, i in enumerate(consumer):
     with s3.open("s3://stockmarket-final-data1/stock_market_{}.json".format(count), 'w') as file:  
         json.dump(i.value, file)    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
